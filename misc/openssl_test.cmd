2198718396f3a880977c969902ebf2dc64b9cc03
knutaf <knutaf@gmail.com>
2012-05-17 21:15:52 +0000

    more debugging in openssl test

--- misc/openssl_test.cmd
+++ misc/openssl_test.cmd
@@ -1,1 +1,1 @@
-openssl s_client -connect localhost:8879 -debug -pause -msg -legacy_renegotiation -tls1 -tlsextdebug
+openssl s_client -connect localhost:8879 -debug -state -pause -msg -legacy_renegotiation -tls1 -tlsextdebug
