02ac86b4cc71f3d2ff0fe54404c6d15242f3bb79
knutaf <knutaf@gmail.com>
2012-06-06 09:22:21 +0000

    allow for separate read-only and read-write accessors

--- inc/mtls_defs.h
+++ inc/mtls_defs.h
@@ -5,9 +5,15 @@
 namespace MungeTLS
 {
 
-#define ACCESSORS(returnType, name, member) \
+#define ACCESSOR_RO(returnType, name, member) \
     const returnType name() const { return member; } \
+
+#define ACCESSOR_RW(returnType, name, member) \
     returnType name() { return const_cast<returnType>(static_cast<std::remove_pointer<decltype(this)>::type const*>(this)->name()); } \
+
+#define ACCESSORS(returnType, name, member) \
+    ACCESSOR_RO(returnType, name, member) \
+    ACCESSOR_RW(returnType, name, member) \
 
 typedef std::vector<BYTE> ByteVector;
 
