f1a0729f58029d564b06a92c3de05b55e35cf875
knutaf <knutaf@gmail.com>
2013-03-22 04:14:04 +0000

    cmd to cleanup certificates slightly modify readme to advise cleaning up certs.

--- misc/add_certs.cmd
+++ misc/add_certs.cmd
@@ -1,9 +1,8 @@
 set makecert=makecert
-certutil -delstore -user -v ROOT mtls-root
+cmd /c %~dp0\del_certs.cmd
+
 %makecert% -pe -r -sr CurrentUser -ss ROOT -n "CN=mtls-root" -a sha1 -sky exchange -cy authority -len 2048 -b 04/01/2010 mtls-root.cer
 
-certutil -delstore -user -v MY mtls-mid
 %makecert% -pe -sr CurrentUser -ss MY -n "CN=mtls-mid" -in "mtls-root" -ir CurrentUser -is ROOT -a sha1 -sky exchange -cy authority -len 2048 -b 04/01/2010 mtls-mid.cer
 
-certutil -delstore -user -v MY mtls-test
 %makecert% -pe -sr CurrentUser -ss MY -n "CN=mtls-test" -in "mtls-mid" -ir CurrentUser -is MY -a sha1 -sky exchange -cy end -len 2048 -b 04/01/2010 mtls-test.cer
---
+++ misc/del_certs.cmd
@@ -0,0 +1,3 @@
+certutil -delstore -user -v ROOT mtls-root
+certutil -delstore -user -v MY mtls-mid
+certutil -delstore -user -v MY mtls-test
